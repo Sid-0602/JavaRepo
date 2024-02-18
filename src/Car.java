704313ccf60f1f056c31824ba920b19d92e9829b
Siddhant_J <sidbnjadhav@gmail.com>
2024-02-18 15:14:19 +0530

    Constructors and Getters/Setters

--- src/Car.java
+++ src/Car.java
@@ -16,10 +16,84 @@ public class Car {
         this.doors = doors;
     }
 
+    //Using multiple constructors depending on the parameters passed into const:
+    public Car(){
+        name="YET TO BE SET";
+        model = "YET TO BE SET";
+        color = "YET TO BE SET";
+        doors = 0;
+    }
+
+    public Car(String name){
+        this.name = name;
+        model = "YET TO BE SET";
+        color = "YET TO BE SET";
+        doors = 0;
+    }
+
+    public Car(String name, String model){
+        this.name = name;
+        this.model = model;
+        color = "YET TO BE SET";
+        doors = 0;
+    }
+
+    public Car(String name, String model, String color){
+        this.name = name;
+        this.model = model;
+        this.color = color;
+        doors = 0;
+    }
+
+    //THESE ARE METHODS Inside Car class.
+
     public void describe_Car(){
         System.out.println("The car: "+this.name);
         System.out.println("Car model is: " + this.model);
         System.out.println("Car's color is: " + this.color);
         System.out.println("Car has doors: " + this.doors);
     }
+
+    public void move(){
+        System.out.println("The car "+ this.name + " is moving");
+    }
+
+    public void stop(){
+        System.out.println("The car " + this.name + " has stopped");
+    }
+    
+    //getters and setter for private attributes: 
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String name){
+        this.name= name;
+        return;
+    }
+    
+    public String getModel(){
+        return model;
+    }
+    
+    public void setModel(String model){
+        this.model = model;
+        return;
+    }
+    
+    public String getColor(){
+        return color;
+    }
+    public void setColor(String color){
+        this.color = color;
+        return;
+    }
+    
+    public int getDoors(){
+        return doors;
+    }
+    public void setDoors(int doors){
+        this.doors = doors;
+    }
+    
 }
--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,42 @@ public class Main {
 
         Car car1 = new Car("Tesla", "S PLADE","white",4); //this is an object created on basis of class "CAR"
         Car car2 = new Car("Ford","MUstang","Red",4);
+        Car car3  = new Car(); //no parameters, hence an default constructor is called.
+
+
+        //getters used for extracting attributes:
+
+        car1.move();
+        car1.stop();
+        System.out.println(car1.getName());
+        System.out.println(car1.getModel());
+        System.out.println(car1.getColor());
+        System.out.println(car1.getDoors());
+        System.out.println("\n");
+
+        car2.move();
+        car2.stop();
+        System.out.println(car2.getName());
+        System.out.println(car2.getModel());
+        System.out.println(car2.getColor());
+        System.out.println(car2.getDoors());
+        System.out.println("\n");
+
+        //setter used to change car name:
+        System.out.println("The car1 name is changed to: " + car1.getName());
+        car1.setName("Mercedes");
+        System.out.println("The car1 name after changing: " + car1.getName());
+
+        System.out.println("\n");
+
+        car3.move();
+        car3.stop();
+        System.out.println(car3.getName());
+        System.out.println(car3.getModel());
+        System.out.println(car3.getColor());
+        System.out.println(car3.getDoors());
+        System.out.println("\n");
 
     }
-
-
 
 }
